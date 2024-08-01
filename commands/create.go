ac878b3742a0b10919f4fcf0975b0abd7041d69a
Andrea Funtò <dihedron.dev@gmail.com>
2024-08-01 14:40:15 +0200

    Add unt test and fix it after project reorganisation

--- commands/create.go
+++ commands/create.go
@@ -10,21 +10,21 @@ import (
 
 type Create struct {
 	Command
-	Source string `short:"s" long:"source" description:"The source filesystem object." required:"yes"`
-	Target string `short:"m" long:"mountpoint" description:"The target mountpoint." required:"yes"`
+	Target     string `short:"t" long:"target" description:"The target filesystem object." required:"yes"`
+	Mountpoint string `short:"m" long:"mountpoint" description:"The mountpoint linking to the target." required:"yes"`
 }
 
 func (cmd *Create) Execute(args []string) error {
 
-	if err := junction.Create(cmd.Source, cmd.Target); err != nil {
-		slog.Error("error creating junction", "source", cmd.Source, "mountpoint", cmd.Target, "error", err)
+	if err := junction.Create(cmd.Target, cmd.Mountpoint); err != nil {
+		slog.Error("error creating junction", "target", cmd.Target, "mountpoint", cmd.Mountpoint, "error", err)
 		return err
 	}
-	result, err := os.Readlink(cmd.Target)
+	result, err := os.Readlink(cmd.Mountpoint)
 	if err != nil {
-		slog.Error("error reading junction", "mountpoint", cmd.Target, "error", err)
+		slog.Error("error reading junction", "mountpoint", cmd.Mountpoint, "error", err)
 		return err
 	}
-	fmt.Printf("%s is linked to %s", cmd.Target, result)
+	fmt.Printf("%s is linked to %s", cmd.Mountpoint, result)
 	return nil
 }
--- junction/junction_test.go
+++ junction/junction_test.go
@@ -7,38 +7,37 @@ import (
 	"testing"
 )
 
-func testCreate() error {
+func TestCreate(t *testing.T) {
 	target, err := os.Getwd()
 	if err != nil {
-		return fmt.Errorf("os.Getwd: %w", err)
+		t.Fatal(fmt.Errorf("os.Getwd: %w", err))
 	}
-	mountPt := filepath.Join(os.TempDir(), "junctionTest")
 
-	err = Create(target, mountPt)
-	if err != nil {
-		return fmt.Errorf("Create: %w", err)
-	}
-	defer os.Remove(mountPt)
+	t.Logf("target is %q", target)
+	mountpoint := filepath.Join(os.TempDir(), "junctionTest")
+	t.Logf("mountpoint is %q", mountpoint)
 
-	link, err := os.Readlink(mountPt)
+	err = Create(target, mountpoint)
 	if err != nil {
-		return fmt.Errorf("os.Readlink: %w", err)
+		t.Fatal(fmt.Errorf("Create: %w", err))
 	}
+	t.Log("mountpoint created")
+	defer os.Remove(mountpoint)
+
+	link, err := os.Readlink(mountpoint)
+	if err != nil {
+		t.Fatal(fmt.Errorf("os.Readlink: %w", err))
+	}
+	t.Log("hard link read")
 	if target != link {
-		return fmt.Errorf("os.Readlink: linked path differs : '%s' != '%s'", target, link)
+		t.Fatal(fmt.Errorf("os.Readlink: linked path differs : '%s' != '%s'", target, link))
 	}
 
-	main_go := filepath.Join(mountPt, "main.go")
-	fd, err := os.Open(main_go)
+	file := filepath.Join(mountpoint, "junction.go")
+	fd, err := os.Open(file)
 	if err != nil {
-		return fmt.Errorf("mount may be fake because '%s' can not be open", main_go)
+		t.Fatal(fmt.Errorf("mount may be fake because '%s' can not be open", file))
 	}
-	fd.Close()
-	return nil
-}
-
-func TestCreate(t *testing.T) {
-	if err := testCreate(); err != nil {
-		t.Fatal(err.Error())
-	}
+	t.Log("file checked")
+	defer fd.Close()
 }
